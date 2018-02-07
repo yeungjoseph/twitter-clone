3e04b85fc001bf2f087b074c7390129e4dccee7f
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-07 11:26:30 -0800

    Add tweets to database

--- models/tweet.js
+++ models/tweet.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
     author: { type: String, required: true },
+    handle: { type: String, required: true },
     content: { type: String, required: true },
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var tweetModel = require('../models/tweet');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('main');
+});
+
+router.post('/tweet', function(req, res) {
+  var newTweet = new tweetModel({
+    author: req.body.author,
+    handle: req.body.handle,
+    content: req.body.content,
+  });
+  newTweet.save(function(err, page) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+    res.end();
+  });
 });
 
 module.exports = router;

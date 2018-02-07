9c8ae7a8d9f72d8007256ba789c0324be669d023
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-07 10:54:48 -0800

    Frontend prototype finish and integrate with express backend

---
+++ models/tweet.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema({
+    author: { type: String, required: true },
+    content: { type: String, required: true },
+});
+
+module.exports = mongoose.model('tweets', schema);

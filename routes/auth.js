77876886fba328e736dcdee57de0d98ede4c2d5e
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-14 11:39:13 -0800

    Encrypt user password on server

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var userModel = require('../models/user');
 
 var router = express.Router();
+
+const saltRounds = 10;
 
 /* Begin routes */
 router.get('/login', function(req, res) {
@@ -12,29 +15,40 @@ router.post('/login', function(req, res) {
   userModel.findOne({ email: req.body.email }, function (err, user) {
     if (err) 
         return res.status(500).send(err);
-    if (user == null || req.body.password != user.password)
+    
+    if (user != null) {
+        bcrypt.compare(req.body.password.trim(), user.password,
+            function(err, success) {
+                if (err) return res.status(500).send(err);
+                if (success) {
+                    req.session.user = user;
+                    return res.redirect('/');
+                }
+                return res.render('login', { login_error: 'Incorrect email or password.' });
+            });
+    }  
+    else 
         res.render('login', { login_error: 'Incorrect email or password.' });
-    else {
-        req.session.user = user;
-        res.redirect('/');
-    }
   });
 });
 
 router.post('/user', function(req, res) {
-    var newUser = new userModel({
-        display_name: req.body.display_name,
-        handle: req.body.handle,
-        email: req.body.email,
-        password: req.body.password
-    });
-    newUser.save(function(err, user) {
-        if (err) {
-            console.log(err);
-            return res.render('login', { reg_error: 'Username or email already taken!' });
-        }
-        req.session.user = user;
-        res.redirect('/');
+    bcrypt.hash(req.body.password.trim(), saltRounds,
+        function(err, hash) {
+            var newUser = new userModel({
+                display_name: req.body.display_name,
+                handle: req.body.handle.trim(),
+                email: req.body.email.trim(),
+                password: hash
+            });
+            newUser.save(function(err, user) {
+                if (err) {
+                    console.log(err);
+                    return res.render('login', { reg_error: 'Username or email already taken!' });
+                }
+                req.session.user = user;
+                res.redirect('/');
+            });    
     });
 });
 

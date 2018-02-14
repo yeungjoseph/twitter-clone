0084402282dac7a632028483eca3c5a88b36ca83
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-13 21:12:51 -0800

    Create separate js file for likes

---
+++ public/javascripts/likes.js
@@ -0,0 +1,39 @@
+$(function(){
+    $(".post").on('click', '.btn-like', function(e) {
+        var button = $(this);
+        var likes = Number(button.attr("numlikes"));
+        var tweetID = button.parent().attr("tweetID");
+        var handle = $(".user-handle").text();
+
+        $.ajax({
+            method: 'POST',
+            url: `/tweet/like/${tweetID}`,
+            data : { handle: handle }
+        })
+        .done(function() {
+            button.attr("numlikes", likes + 1);
+            button.html(`<span class="fas fa-heart red"></span> ${likes+1}`);
+            button.removeClass('btn-like');
+            button.addClass('btn-unlike');
+        });		
+    });
+
+    $(".post").on('click', '.btn-unlike', function(e) {
+        var button = $(this);
+        var likes = Number(button.attr("numlikes"));
+        var tweetID = button.parent().attr("tweetID");
+        var handle = $(".user-handle").text();
+
+        $.ajax({
+            method: 'DELETE',
+            url: `/tweet/like/${tweetID}`,
+            data: { handle: handle }
+        })
+        .done(function() {
+            button.attr("numlikes", likes - 1);
+            button.html(`<span class="far fa-heart red"></span> ${likes-1}`);
+            button.removeClass('btn-unlike');
+            button.addClass('btn-like');
+        });
+    });
+});
